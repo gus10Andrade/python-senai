9dd9bb3328c281deb07e65a42b5f8a783299373c
gus10Andrade <gustavo.gpm2810@gmail.com>
2024-12-03 09:48:01 -0300

    Aula 2

---
+++ Aula#2/aula02.py
@@ -0,0 +1,11 @@
+print ('Bom dia a todos')
+
+nome_usuario = 'Gustavo Andrade' # tipo -> str = string
+Idade_usuario = 21 # tipos -> = int = interger
+altura_usuario = 1.77 # tipo -> float = ponto flutuante
+maior_usuario = True # tipos -> bool = boolean
+
+print ('Seja bem-vindo', nome_usuario)
+print ('sua idade', Idade_usuario, 'anos') 
+print ('sua altura', altura_usuario, 'metros')
+print ('voce é maior de idade', maior_usuario)
